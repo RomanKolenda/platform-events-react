422e52b5456d188861fa85256d426ad4c366b980
Roman Kolenda <kolenda.roman@yandex.ru>
2019-05-24 10:21:36 +0300

    added axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import { configureCometD } from './cometdConnection'
+import axios from 'axios';
 
 class App extends Component {
-
+  state = {};
   componentDidMount() {
-    configureCometD(
+    /* configureCometD(
       this.refs.app,
       'https:',
       'roman-kolenda-itechart-dev-ed.lightning.force.com',
       '00D2v000000R3WR!AQcAQGmR0h6hVao9SHL.xqYAF7HChalChDF7ZOnALPyBmRBTkdw_5gAMp8uMJz2Fzm6GxyLvcLY3MGHDmx.P36eVTxcpgVdv'
-    )
+    ) */
+      axios.get('https://login.salesforce.com')
+        .then(Response => {
+          console.log(Response);
+        });
   }
 
   render() {
